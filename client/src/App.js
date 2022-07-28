a2ab7afaa6ade357132298a07e5ca8712d5b5b63
Gabriel Rocha <gabrielrs7@outlook.com>
2022-07-28 20:47:47 -0300

    feat: creating UserAdvertiser component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import Footer from "./components/footer";
 import { Input } from "./components/input";
+import UserAdvertiser from "./components/userAdvertiser";
 import Router from "./routes";
 import { GlobalStyle } from "./styles/globalstyles";
 
@@ -10,6 +11,7 @@ function App() {
       <Router/>
       <Input label={'label'} placeholder={'placeholder'} name={'label'} type={'password'}/>
       <Input label={'Textarea'} placeholder={'placeholder'} name={'label'} isTextarea={true}/>
+      <UserAdvertiser userName={'Gabriel   '} color='#f0f'/>
       <Footer/>
     </>
   );
---
+++ client/src/components/userAdvertiser/index.jsx
@@ -0,0 +1,17 @@
+import { UserAdvertiserContainer } from "./style"
+
+
+const UserAdvertiser = ({userName, color, isWhite=false})=>{
+    userName = userName.split(' ').filter(word=>word.length).join(' ')
+    return(
+        <UserAdvertiserContainer color={color} isWhite={isWhite}>
+            <div className="circle">
+                {userName.split(' ').reduce((acc, letter)=>acc+letter.toUpperCase()[0],'').slice(0,2)}
+            </div>
+            <span>{userName}</span>
+
+        </UserAdvertiserContainer>
+    )
+}
+
+export default UserAdvertiser
