7258088ef4e4ce3c38d56487df4b91b301cebafa
Felipe Braga Alfredo <babiferreirab15@gmail.com>
2022-07-28 14:27:48 +0200

    feat:pages

--- client/src/pages/product/index.js
+++ client/src/pages/product/index.js
@@ -1,6 +1,14 @@
 import Header from "../../components/header";
-
+import { SecundaryHeader } from "./style";
 const PageProducts = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <SecundaryHeader>
+        <h1>Velocidade e experiencia em um lugar feito para voce</h1>
+        <p>Um ambiente feito para voce explorar o seu melhor</p>
+      </SecundaryHeader>
+    </>
+  );
 };
 export default PageProducts;
